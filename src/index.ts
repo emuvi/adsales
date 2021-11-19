8af178fda63cc6ce9a293120f9d92592a558e645
Éverton M. Vieira <everton.muvi@gmail.com>
2021-11-19 18:42:51 -0300

    prototype development

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-import { QinColumn, QinLabel } from "qinpel-cps"
+import { QinButton, QinColumn, QinLabel } from "qinpel-cps"
 
 class AdSales extends QinColumn {
 
     private qinHello: QinLabel = new QinLabel("Hello, AdSales!");
+    private qinPeople: QinButton = new QinButton(null, new QinLabel("People"));
 
     public constructor() {
         super();
         this.qinHello.install(this);
+        this.qinPeople.install(this);
+        this.qinPeople.addAction(qinEvent => {
+            if (qinEvent.isPrimary()) {
+                const frame = this.qinpel().manager.newFrame("Search People", "adpeople",
+                    { search: "people" });
+                frame.addWaiter(result => {
+                    console.log("Res: " + result);
+                    frame.close();
+                });
+            }
+        });
     }
 
 }
