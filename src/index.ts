408d5705bb1e912e8525171d1c9bac57ae48055d
Éverton M. Vieira <everton.muvi@gmail.com>
2022-01-06 11:14:25 -0300

    small updates

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { QinButton, QinColumn, QinLabel } from "qinpel-cps"
 class AdSales extends QinColumn {
 
     private qinHello: QinLabel = new QinLabel("Hello, AdSales!");
-    private qinPeople: QinButton = new QinButton(null, new QinLabel("People"));
+    private qinPeople: QinButton = new QinButton({ label: new QinLabel("People") });
 
     public constructor() {
         super();
