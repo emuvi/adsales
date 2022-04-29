393b6be10a807afe6aaa3c340dc96c64bb851b94
Éverton M. Vieira <everton.muvi@gmail.com>
2022-04-29 12:34:03 -0300

    prototype development

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
-import { QinButton, QinColumn, QinLabel } from "qinpel-cps"
+import { QinButton, QinColumn, QinLabel } from "qinpel-cps";
 
 class AdSales extends QinColumn {
+  private qinHello: QinLabel = new QinLabel("Hello, AdSales!");
+  private qinPeople: QinButton = new QinButton({ label: new QinLabel("People") });
 
-    private qinHello: QinLabel = new QinLabel("Hello, AdSales!");
-    private qinPeople: QinButton = new QinButton({ label: new QinLabel("People") });
-
-    public constructor() {
-        super();
-        this.qinHello.install(this);
-        this.qinPeople.install(this);
-        this.qinPeople.addAction(qinEvent => {
-            if (qinEvent.isMain) {
-                const frame = this.qinpel.manager.newFrame("Search People", "adpeople",
-                    { search: "people" });
-                frame.addWaiter(result => {
-                    console.log("Res: " + result);
-                    frame.close();
-                });
-            }
+  public constructor() {
+    super();
+    this.qinHello.install(this);
+    this.qinPeople.install(this);
+    this.qinPeople.addAction((qinEvent) => {
+      if (qinEvent.isMain) {
+        const frame = this.qinpel.chief.newFrame("Search People", "adpeople", {
+          search: "people",
         });
-    }
-
+        frame.addWaiter((result) => {
+          console.log("Res: " + result);
+          frame.close();
+        });
+      }
+    });
+  }
 }
 
 new AdSales().style.putAsBody();
